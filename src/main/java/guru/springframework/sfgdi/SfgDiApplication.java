95cff1bc2608f5339587cd3797f88bd312c66315
Mohd Zhafri <mohdzhafri.z@maybank.com>
2021-01-25 18:24:49 +0800

    Modified setter based DI to use spring annotation.

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.MyController;
 import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,13 +14,15 @@ public class SfgDiApplication {
 		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
 		MyController myController = (MyController) context.getBean("myController");
 		String greeting = myController.sayHello();
-
 		System.out.println(greeting);
 
 		System.out.println("Property based injection");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) context.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("Setter based injection");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) context.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
 	}
 
 }
