e72eeb3bffca3a5f6c51cc04793bc36d7696f698
Mohd Zhafri <mohdzhafri.z@maybank.com>
2021-01-26 07:59:55 +0800

    Default profile.

--- src/main/java/guru/springframework/sfgdi/services/Il8nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/Il8nEnglishGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("il8nService")
-@Profile("EN")
+@Profile({"EN", "default"})
 public class Il8nEnglishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
